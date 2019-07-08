ea2fb72e63774532210f4469a3a1f2c7b29de8e8
Michael Ernst <mernst@cs.washington.edu>
2019-07-08 14:24:24 -0700

    Replace hard-coded logic by scripts

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -2,7 +2,7 @@
 
 echo "Entering java-getopt/.travis-build-without-test.sh" in `pwd`
 
-# Fail the whole script if any command fails
+# Fail the whole script if any command fails.
 set -e
 
 export SHELLOPTS
@@ -15,30 +15,23 @@ export CHECKERFRAMEWORK=`realpath ../checker-framework`
 
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
-SLUGOWNER=${TRAVIS_PULL_REQUEST_SLUG%/*}
-if [[ "$SLUGOWNER" == "" ]]; then
-  SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
-fi
-if [[ "$SLUGOWNER" == "" ]]; then
-  SLUGOWNER=typetools
-fi
-echo SLUGOWNER=$SLUGOWNER
+git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
+  || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
+SLUGOWNER=`/tmp/plume-scripts/git-organization typetools`
 
 ## Build Checker Framework
+
 if [ -d ../checker-framework ] ; then
     # Older versions of git don't support the -C command-line option
     (cd ../checker-framework && git pull)
 else
-    set +e
-    git ls-remote https://github.com/${SLUGOWNER}/checker-framework.git &>/dev/null
-    if [ "$?" -ne 0 ]; then
-	CFSLUGOWNER=typetools
-    else
-	CFSLUGOWNER=${SLUGOWNER}
-    fi
-    set -e
-    (cd .. && git clone --depth 1 https://github.com/${CFSLUGOWNER}/checker-framework.git)
+  REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework`
+  BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
+  (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework)
+  # This also builds annotation-tools and jsr308-langtools
+  (cd ../checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
 fi
+export CHECKERFRAMEWORK=`readlink -f ../checker-framework`
 
 # This also builds annotation-tools and jsr308-langtools
 (cd ../checker-framework/ && ./.travis-build-without-test.sh downloadjdk jdk8)
