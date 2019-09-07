ac3f34ba623ec0763a94e3eadbf983262a2a5bcb
Michael Ernst <mernst@alum.mit.edu>
2019-09-07 06:57:08 -0700

    Use git-clone-related script

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -17,7 +17,6 @@ export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
 git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
   || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
-SLUGOWNER=`/tmp/plume-scripts/git-organization typetools`
 
 ## Build Checker Framework
 
@@ -25,9 +24,7 @@ if [ -d ../checker-framework ] ; then
     # Older versions of git don't support the -C command-line option
     (cd ../checker-framework && git pull)
 else
-  REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework`
-  BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
-  (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework)
+  /tmp/plume-scripts/git-clone-related typetools checker-framework
   # This also builds annotation-tools and jsr308-langtools
   (cd ../checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
 fi
